3683ee39b8e8874f07d299e9035de96a4a17dbb3
Guan-Yi <13940863771@163.com>
2016-05-20 22:16:18 +0800

    start learning collectionView animation
    build project

---
+++ CollectionViewAnimation/CustomFlowLayout.swift
@@ -0,0 +1,13 @@
+//
+//  CustomFlowLayout.swift
+//  CollectionViewAnimation
+//
+//  Created by YGuan on 5/20/16.
+//  Copyright © 2016 YGuan. All rights reserved.
+//
+
+import UIKit
+
+class CustomFlowLayout: UICollectionViewFlowLayout {
+    
+}
---
+++ CollectionViewAnimation/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CollectionViewAnimation
+//
+//  Created by YGuan on 5/20/16.
+//  Copyright © 2016 YGuan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
